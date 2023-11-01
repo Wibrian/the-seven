8f3dfc3dec9e4252f9b619b129ca6a7575109f4d
Wibrian Alfathan <wibrianalfathan@gmail.com>
2023-11-01 17:13:52 +0700

    Update for Responsive

--- main.js
+++ main.js
@@ -15,30 +15,5 @@ togBtn.addEventListener("click", () => {
   }
 });
 
-const playBtn = document.getElementById("play");
-const audio = document.getElementById("audio");
-
-// const duration = document.getElementById("audio").duration;
-
-console.log(audio.duration);
-
-let count = 0;
-
-playBtn.addEventListener("click", (event) => {
-  if (count == 0) {
-    count = 1;
-    audio.play();
-    playBtn.innerHTML = '<i class="fa-solid fa-circle-stop"></i>';
-  } else if (count == 1) {
-    count = 0;
-    audio.pause();
-    audio.currentTime = 0;
-    playBtn.innerHTML = '<i class="fa-solid fa-circle-play"></i>';
-  }
-  // else if (count == 1 && audio != event.target) {
-  //   count = 0;
-  //   audio.pause();
-  //   audio.currentTime = 0;
-  //   playBtn.innerHTML = '<i class="fa-solid fa-circle-play"></i>';
-  // }
-});
+// const header = document.querySelector("header");
+// const sectionOne = document.querySelector("main");
